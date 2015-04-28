513f5a917f9b06aa6ff976ed450116f63d7f4488
Camille Hodoul <camille.hodoul@gmail.com>
2015-04-28 23:39:27 +0200

    export hexToRgba()

--- src/rgbatokml.js
+++ src/rgbatokml.js
@@ -84,6 +84,7 @@ var converter = (function() {
 exports.rgbaToHex = converter.rgbaToHex;
 exports.rgbaToKml = converter.rgbaToKml;
 exports.kmlToRgba = converter.kmlToRgba;
+exports.hexToRgba = converter.hexToRgba;
 if(typeof module !== "undefined") {
     module.exports = exports;
 }
